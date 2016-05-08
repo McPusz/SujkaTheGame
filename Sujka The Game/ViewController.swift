a2ff4a63b3b5b4855ddca687cbf01815e006456e
McPusz <mcpusz@mcpuszs-macbook-pro.local>
2016-05-09 00:30:08 +0200

    Obrazki wstawione

--- Sujka The Game/ViewController.swift
+++ Sujka The Game/ViewController.swift
@@ -13,11 +13,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+      assignbackground()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func assignbackground(){    /* tlo */
+        
+        let background = UIImage(named: "background")
+        
+        var imageView : UIImageView!
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode = UIViewContentMode.ScaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = background
+        imageView.center = view.center
+        view.addSubview(imageView)
+        self.view.sendSubviewToBack(imageView)
     }
 
 
