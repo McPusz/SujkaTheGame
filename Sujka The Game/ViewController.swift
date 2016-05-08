d0c6dfe44e3b81a00018b57c1683d647dd031618
McPusz <mcpusz@mcpuszs-macbook-pro.local>
2016-05-09 00:55:11 +0200

    iii wszystko bangla :>

--- Sujka The Game/ViewController.swift
+++ Sujka The Game/ViewController.swift
@@ -9,11 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var scoreLabel : UILabel!
+    @IBOutlet var timerLabel : UILabel!
+    
+    var count = 0
+    var seconds = 0
+    var timer = NSTimer()
+    
+    @IBAction func buttonPressed(){ /* nacisniecie guzika */
+        count += 1
+        scoreLabel.text = "Score \n\(count)"
+        
+        NSLog("Button Pressed")
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
       assignbackground()
+        setupGame()
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,7 +51,36 @@ class ViewController: UIViewController {
         view.addSubview(imageView)
         self.view.sendSubviewToBack(imageView)
     }
-
-
+    
+    func setupGame(){   /* setting the game */
+        seconds = 30
+        count = 0
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target:self, selector: Selector("subtractTime"), userInfo: nil, repeats: true)
+        
+        timerLabel.text = "Time: \(seconds)"
+        scoreLabel.text = "Score: \(count)"
+        
+    }
+    
+    func subtractTime(){    /* decrement seconds, update time, alert pop */
+        seconds -= 1
+        timerLabel.text = "Time: \(seconds)"
+        
+        if (seconds == 0){
+            timer.invalidate()
+            
+            let alert = UIAlertController(title: "Starczy luju!", message: "Wypilas \(count) browcow. Dumna jestes z siebie?", preferredStyle: UIAlertControllerStyle.Alert)
+            
+            alert.addAction(UIAlertAction(title: "Nie ;____;", style:  UIAlertActionStyle.Default, handler : {
+                action in self.setupGame()
+            }))
+            
+            
+            presentViewController(alert, animated: true, completion: nil)
+        }
+        
+        
+    }
 }
 
